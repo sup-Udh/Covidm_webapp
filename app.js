8a067de5a99c2043eff51663ad4ccddc596f1ee6
Udhay <udhay1co.de@gmail.com>
2021-08-27 21:19:42 +0530

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const port = 3000;
+const port =  process.env.PORT ||  3000;
 const fs = require('fs')
 const mysql = require('mysql');
 const ejs  = require('ejs');
