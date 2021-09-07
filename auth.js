80d251f6e906824be35ae44bafbd2685355f2003
Udhay <udhay1co.de@gmail.com>
2021-09-07 02:55:36 +0000

    git hub login

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+
 
 
 const GOOGLE_CLIENT_ID = '738177854751-ith7ms5rfv3sfkt4vtnra4vh5bk779dr.apps.googleusercontent.com';
@@ -9,7 +11,7 @@ passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: process.env.API_KEY,
   // switch back later on to the main heroku route (IMP REMINDER
-  callbackURL: "http://localhost:3000/google/callback",
+  callbackURL: "https://covidm-beta.herokuapp.com/google/callback",
   passReqToCallback: true,
 
 
@@ -28,3 +30,13 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
+
+passport.use(new GitHubStrategy({
+  clientID: '45b8c373721feb005300',
+  clientSecret: process.env.CLIENT_SECERT,
+  callbackURL: "https://covidm-beta.herokuapp.com/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  return done(null, profile);
+}
+));
