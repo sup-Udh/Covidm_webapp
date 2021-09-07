ca8f4e32ccddc67737574edb6899fc1fac3bd997
Udhay <udhay1co.de@gmail.com>
2021-09-07 03:08:29 +0000

    app.js

--- app.js
+++ app.js
@@ -149,9 +149,9 @@ app.get('/auth/failure' , isloggedIn ,  (req,res) => {
 app.get('/auth/github',passport.authenticate('github',{ scope: [ 'user:email' ] }));
 
 app.get('/auth/github/callback',passport.authenticate('github', {
+ successRedirect: '/user',
  failureRedirect: '/auth/failure',
- failureRedirect: '/auth/failure',
- 
+
 } )
 )
 
